b6a40fd39b5164214b46502d30be5db47a2d8152
Dimitrie Stefanescu <didimitrie@gmail.com>
2018-12-21 15:44:37 +0000

    updated build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.842d14fa02c433b50e97938d8a3964e6.js"
+  "/precache-manifest.49e1d77103b33ae6ab7e5bc554d8b92e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "speckle-admin"});
