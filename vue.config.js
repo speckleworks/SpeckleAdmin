a118b3ca8c17e41266b369375af7583f32759cd0
Mishael Nuh <mishael.nuh@arup.com>
2019-06-25 18:02:23 +0100

    Add proxy to allow for local debug.

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,15 @@ module.exports = {
   pwa: {
     name: 'Speckle',
     themeColor: '#0962FF'
+  },
+
+  devServer: {
+    proxy: {
+      '^/.netlify/functions/*': {
+        target: 'http://localhost:9000',
+        pathRewrite: function (path, req) { return path.replace('/.netlify/functions', '') },
+        changeOrigin: true
+      }
+    }
   }
 }
