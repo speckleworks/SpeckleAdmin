60902736edb64e70729a0a661222adf9f22d5d78
Matthew Swaidan <matthew.swaidan@woodsbagot.com>
2019-07-17 20:56:06 -0400

    Adds archive / delete to AdminProjects

--- src/views/AdminProjects.vue
+++ src/views/AdminProjects.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout row wrap>
-    <v-flex xs12>
+    <v-flex xs12 md9>
       <v-text-field
           solo
           v-model="search"
@@ -9,6 +9,15 @@
           single-line
           hide-details
         ></v-text-field>
+    </v-flex>
+    <v-flex xs12 md1>
+      <v-btn :disabled="buttonsDisabled" flat class='transparent' @click="archiveSelected(true)">Archive</v-btn>
+    </v-flex>
+    <v-flex xs12 md1>
+      <v-btn :disabled="buttonsDisabled" flat class='transparent' @click="archiveSelected(false)">Restore</v-btn>
+    </v-flex>
+    <v-flex xs12 md1>
+      <v-btn :disabled="buttonsDisabled" flat color='error' class='transparent' @click='showWarning = true'>Delete</v-btn>
     </v-flex>
     <v-flex xs12>
       <v-data-table
@@ -35,6 +44,9 @@
             <td >{{ props.item.private }}</td>
             <td >{{ props.item.permissions.canRead.length + 1 }}</td>
             <td >{{ props.item.permissions.canWrite.length + 1}}</td>
+            <td >
+              <v-checkbox disabled hide-details class="align-center justify-left" v-model=props.item.deleted></v-checkbox>
+            </td>
             <td>
               <v-btn small color='primary' :to='"/projects/"+props.item._id'>Details</v-btn>
             </td>
@@ -42,6 +54,19 @@
         </template>
       </v-data-table>
     </v-flex>
+    <v-dialog v-model="showWarning" max-width="500">
+      <v-card>
+        <v-card-title>
+          <span class="headline font-weight-light"><strong>Permanently</strong> delete these projects?</span>
+          <v-progress-linear color='error' indeterminate v-show='showDeleteProgress'/>
+        </v-card-title>
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn flat color='error' class='transparent' @click="deleteSelected()">Delete Permanently</v-btn>
+          <v-btn @click="showWarning = false">Cancel</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-layout>
 </template>
 <script>
@@ -57,17 +82,17 @@ export default {
   watch: {
   },
   computed: {
-    projects( ) {
-      return this.projectsResource.filter(project => project.deleted === false ).sort( ( a, b ) => {
-        return new Date( b.updatedAt ) - new Date( a.updatedAt );
-      } )
-    },
-    
+    buttonsDisabled(){
+      if (this.selected.length > 0){
+        return false
+      }
+      return true
+    }
 
   },
   data( ) {
     return {
-      projectsResource: [],
+      projects: [],
       isGettingProjectData: false,
       selected: [],
       search: "",
@@ -76,11 +101,13 @@ export default {
         { text: 'Owner', value: 'owner' },
         { text: 'Streams', value: 'streams.length'},
         { text: 'Private', value: 'private' },
-        { text: 'Read Access Users:', value: 'permissions.canRead.length'},
-        { text: 'Write Access Users:', value: 'permissions.canWrite.length'},
-        { text: 'Actions', value: ''}
-
+        { text: 'Read Users:', value: 'permissions.canRead.length'},
+        { text: 'Write Users:', value: 'permissions.canWrite.length'},
+        { text: "Archived", value: "deleted"},
+        { text: '', value: ''}
       ],
+      showWarning: false,
+      showDeleteProgress: false
     }
   },
   methods: {
@@ -88,7 +115,7 @@ export default {
       this.isGettingProjectData = true
       Axios.get( 'projects/admin' )
         .then( res => {
-          this.projectsResource = res.data.resources
+          this.projects = res.data.resources
           this.isGettingProjectData = false
         } )
         .catch( err => {
@@ -108,6 +135,39 @@ export default {
           console.error( err )
           return "Coundn't get name"
         } )
+    },
+    archiveSelected(boolean){
+      let projectsToModify = this.selected.length
+      this.selected.forEach(project => {
+        Axios.put("projects/" + project._id, {deleted: boolean } ).then(res => {
+          projectsToModify -= 1
+          if (projectsToModify == 0) this.fetchData()
+        })
+        .catch( err => {
+          // TODO: Handle error
+          console.error( err )
+          return "Coundn't archive project"
+        } )
+      })
+    },
+    deleteSelected(){
+      this.showDeleteProgress = true
+      let projectsToDelete = this.selected.length
+      this.selected.forEach(project => {
+        Axios.delete("projects/" + project._id).then(res => {
+          projectsToDelete -= 1
+          if (projectsToDelete == 0) {
+            this.showDeleteProgress = false
+            this.showWarning = false
+            this.fetchData()
+          }
+        })
+        .catch( err => {
+          // TODO: Handle error
+          console.error( err )
+          return "Coundn't delete project"
+        } )
+      })
     }
   },
   mounted( ) {
