6c5422dd931e521759dd248fdcf3ebeb802405fa
mswaidan <mswaidan@gmail.com>
2019-07-23 15:10:06 -0400

    Adds user.role.admin to canEdit project

--- src/components/UserPermissionProjectCard.vue
+++ src/components/UserPermissionProjectCard.vue
@@ -7,15 +7,15 @@
       </v-flex>
       <v-flex xs4>
         <!-- write streams -->
-        <v-btn block depressed small :color='hasWritePermissionStreams?"primary":""' @click.native='changePermissionStreams()' :disabled='user.surname.includes(`(that is you!)`) || globalDisabled || user.isOwner'>
+        <v-btn block depressed small :color='hasWritePermissionStreams?"primary":""' @click.native='changePermissionStreams()' :disabled='!canEdit'>
           {{ user.isOwner ? "write streams" : hasWritePermissionStreams ? "write streams" : "read streams"}}
         </v-btn>
         <!-- write project -->
-        <v-btn block depressed small :color='hasWritePermissionProject?"primary":""' @click.native='changePermissionProject()' :disabled='user.surname.includes(`(that is you!)`) || globalDisabled || user.isOwner'>
+        <v-btn block depressed small :color='hasWritePermissionProject?"primary":""' @click.native='changePermissionProject()' :disabled='!canEdit'>
           {{ user.isOwner ? "write project" : hasWritePermissionProject ? "write project" : "read project"}}
         </v-btn>
         <!-- remove user -->
-        <v-btn block flat small @click.native='removeUser()' :disabled='user.surname.includes(`(that is you!)`) || globalDisabled || user.isOwner'>
+        <v-btn block flat small @click.native='removeUser()' :disabled='!canEdit'>
           remove user
         </v-btn>
       </v-flex>
@@ -31,11 +31,14 @@ export default {
     globalDisabled: Boolean,
   },
   computed: {
+    canEdit(){
+      return this.user.surname.includes(`(that is you!)`) || this.globalDisabled || this.user.isOwner || this.user.role === 'admin'
+    },
     hasWritePermissionStreams(){
       return this.project.permissions.canWrite.indexOf( this.user._id ) > -1
     },
     hasWritePermissionProject(){
-      return this.project.canWrite.indexOf( this.user._id ) > -1
+      return this.project.canWrite.indexOf( this.user._id ) > -1 
     }
   },
   data( ) { return {} },
