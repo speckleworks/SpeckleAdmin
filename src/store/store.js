834968f429b733257d869b770dd10218e3b2e5a3
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-10-03 13:38:36 +0100

    refactor(store): moved out utility functions, fixed loading issues

--- src/store/store.js
+++ src/store/store.js
@@ -9,7 +9,12 @@ import flatten from 'flat'
 
 import { LambdaSettings } from './lambda/_lambdaSettings.js'
 
+import { removeArraysRecursive, getStructuralArrPropKeys, getTokenMSAL } from './utilities.js'
+
 Vue.use( Vuex )
+
+import admin from './admin.js'
+import viewer from './viewer.js'
 
 export default new Vuex.Store( {
   state: {
@@ -1048,76 +1053,7 @@ export default new Vuex.Store( {
     }
   },
   modules: {
-    admin: import( './admin.js' ),
-    viewer: import( './viewer.js' ),
+    admin: admin,
+    viewer: viewer,
   }
 } )
-
-function removeArraysRecursive( foo ) {
-  let bar = {}
-
-  for ( let key in foo ) {
-    if ( !foo.hasOwnProperty( key ) ) continue
-    else if ( Array.isArray( foo[ key ] ) ) {
-      //bar[ key + ' (array)' ] = `Array with ${ foo[key].length } elements`
-    } else if ( typeof foo[ key ] === 'object' && foo[ key ] !== null ) {
-      bar[ key ] = removeArraysRecursive( foo[ key ] )
-    } else {
-      bar[ key ] = foo[ key ]
-    }
-  }
-  return bar
-}
-
-function getStructuralArrPropKeys( foo ) {
-  let bar = {}
-
-  for ( let key in foo ) {
-    if ( !foo.hasOwnProperty( key ) ) continue
-    else if ( Array.isArray( foo[ key ] ) ) {
-      bar[ key ] = `Array with ${ foo[key].length } elements`
-    } else if ( typeof foo[ key ] === 'object' && foo[ key ] !== null ) {
-      bar[ key ] = getStructuralArrPropKeys( foo[ key ] )
-    }
-  }
-  return bar
-}
-
-async function getTokenMSAL( { clientId, authority, loginRequest } ) {
-  // TODO: THIS CANNOT BE CALLED MULTIPLE TIMES!!!
-  // DEPENDS ON LOCAL STORAGE TO PROPERLY OBTAIN CLIENTID
-  var userAgent = new Msal.UserAgentApplication( {
-    auth: {
-      clientId: clientId,
-      authority: authority,
-      redirectUri: window.location.origin + "/msal.html"
-    },
-    cache: {
-      cacheLocation: "localStorage",
-      storeAuthStateInCookie: true
-    },
-  } )
-
-  var token = await userAgent.getCachedToken( clientId )
-
-  if ( token )
-    return token.accessToken
-
-  try {
-    token = await userAgent.acquireTokenSilent( loginRequest )
-  } catch ( e ) {
-    console.log( 'MSAL Error: ' + e.errorCode )
-    if ( e.errorCode === "user_login_error" ) {
-      window.localStorage.msalClientId = clientId
-      await userAgent.loginPopup( loginRequest )
-      delete window.localStorage.msalClientId
-      return await getTokenMSAL( { clientId: clientId, authority: authority, loginRequest: loginRequest } )
-    } else if ( e.errorCode === "token_renewal_error" ) {
-      window.localStorage.msalClientId = clientId
-      token = await userAgent.acquireTokenPopup( loginRequest )
-      delete window.localStorage.msalClientId
-    }
-  }
-
-  return token.accessToken
-}
